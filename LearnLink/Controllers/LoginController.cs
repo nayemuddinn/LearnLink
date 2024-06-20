656daf6504f58a81e82b716ba6462a74c84dfa0d
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2024-06-20 10:26:50 +0600

    Changed the background and added temporary login

--- LearnLink/Controllers/LoginController.cs
+++ LearnLink/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LearnLink.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,26 @@ namespace LearnLink.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
         public ActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            if (user.Email.Equals("abc@gmail.com") && user.Password.Equals("1234"))
+            {
+                Session["User"] = user.Username;
+                return RedirectToAction("Dashboard", "Dashboard");
+
+            }
+            else
+                ViewBag.Log = "Login Failed";
+
+            return View();
+        }
     }
 }
+
+
