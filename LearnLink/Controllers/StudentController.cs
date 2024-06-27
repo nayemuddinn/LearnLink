3dab9684382fb9008a32732c7c9e60d8b3c8b281
Nayrit <110413462+Nayrit@users.noreply.github.com>
2024-06-27 13:08:26 +0600

    manageCourse

---
+++ LearnLink/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿// StudentController.cs
+using System.Linq;
+using System.Web.Mvc;
+using LearnLink.Models;
+
+namespace LearnLink.Controllers
+{
+    public class StudentController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Student/Courses
+        public ActionResult Courses()
+        {
+          //  var courses = db.Courses.ToList();
+            return View();
+        }
+
+        // Other actions for students can be added here
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
