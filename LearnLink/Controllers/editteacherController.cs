ecf4ccd757a4150ec02bdb2dccbf5dd1450da0c7
Nayrit <110413462+Nayrit@users.noreply.github.com>
2024-06-29 21:28:41 +0600

    Dashboard all done and working

---
+++ LearnLink/Controllers/editteacherController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnLink.Controllers
+{
+    public class editteacherController : Controller
+    {
+        // GET: editteacher
+        public ActionResult editProfile()
+        {
+            return View();
+        }
+    }
+}
