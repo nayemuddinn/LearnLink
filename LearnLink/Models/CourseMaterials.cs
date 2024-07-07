9fb9bf871c83af80c9873faba8b699d4fa0a5f29
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2024-07-07 11:22:41 +0600

    Created Material model class

---
+++ LearnLink/Models/CourseMaterials.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnLink.Models
+{
+    public class CourseMaterials
+    {
+        public int MaterialID { get; set; }
+        public int CourseID { get; set; }
+        public string MaterialName { get; set; }
+        public string Content { get; set; }
+    }
+}
