faaf141c5675f8fc62967b55e588aced519af685
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2024-07-07 11:18:37 +0600

    Created Course model class

---
+++ LearnLink/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnLink.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public string CourseDescription { get; set; }
+        public string CoursePrerequisite { get; set; }
+        public int CourseFee { get; set; }
+        public int TeacherID { get; set; }
+        public User Teacher { get; set; }
+    }
+}
