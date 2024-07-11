24d573ef5de5ff0c2e1f9d7c28e5fab13b777147
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2024-07-11 16:35:50 +0600

    Fetching Course Materials from database for particular course Value

--- LearnLink/Controllers/ManageCourseController.cs
+++ LearnLink/Controllers/ManageCourseController.cs
@@ -2,6 +2,7 @@
 using LearnLink.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Web.Mvc;
 
@@ -9,13 +10,13 @@ namespace LearnLink.Controllers
 {
     public class ManageCourseController : Controller
     {
-
+        string constr = DBconnection.connStr;
         public ActionResult ManageCourse()
         {
             int teacherID = (int)Session["UserID"];
             List<Course> courses = new List<Course>();
 
-            string constr = DBconnection.connStr;
+          
             using (SqlConnection con = new SqlConnection(constr))
             {
                 string query = "SELECT CourseID, CourseName,CourseCreateDate FROM Courses WHERE TeacherID = @TeacherID";
@@ -42,7 +43,36 @@ namespace LearnLink.Controllers
             return View(courses); 
         }
 
+        public ActionResult ShowCourseMaterials(int courseid)
+        {
+            List<CourseMaterials> materials = new List<CourseMaterials>();
 
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                string query = "SELECT FileID, CourseID, Name, ContentType, UploadDate FROM CourseMaterials WHERE CourseID = @CourseID";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@CourseID", courseid);
+                    con.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            materials.Add(new CourseMaterials
+                            {
+                                FileID = reader["FileID"] != DBNull.Value ? Convert.ToInt32(reader["FileID"]) : 0,
+                                CourseID = reader["CourseID"] != DBNull.Value ? Convert.ToInt32(reader["CourseID"]) : 0,
+                                Name = reader["Name"] != DBNull.Value ? reader["Name"].ToString() : "No Name",
+                                ContentType = reader["ContentType"] != DBNull.Value ? reader["ContentType"].ToString() : "Unknown",
+                                UploadDate = reader["UploadDate"] != DBNull.Value ? Convert.ToDateTime(reader["UploadDate"]) : DateTime.MinValue
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+            }
+
+            return View(materials);
+        }
     }
 }
-  
