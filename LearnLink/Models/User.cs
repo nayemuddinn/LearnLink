cc73087525179be1da3c691f1377e344effd4e27
Nayrit <nayritarn@gmail.com>
2024-07-12 23:57:37 +0600

    Reconfirm password added

--- LearnLink/Models/User.cs
+++ LearnLink/Models/User.cs
@@ -9,6 +9,7 @@ namespace LearnLink.Models
     {
         public string Name { get; set; }
         public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
