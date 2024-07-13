6c981c2a859137392a2ea7dd5ecd3d2420335a30
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2024-07-13 21:37:21 +0600

    Added Enrolled Courses View class

---
+++ LearnLink/Controllers/StudentEnrolledCoursesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnLink.Controllers
+{
+    public class StudentEnrolledCoursesController : Controller
+    {
+        // GET: StudentEnrolledCourses
+        public ActionResult ViewEnrolledCourses()
+        {
+            return View();
+        }
+    }
+}
