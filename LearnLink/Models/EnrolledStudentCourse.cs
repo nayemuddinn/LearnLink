db9503b49f67cefa9b9cf1aa154a60f91c86007d
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2024-07-14 02:01:55 +0600

    Added AllEnrollment for a teacher

---
+++ LearnLink/Models/EnrolledStudentCourse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnLink.Models
+{
+    public class EnrolledStudentCourse
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string Institution { get; set; }
+        public string Phone { get; set; } 
+    }
+}
