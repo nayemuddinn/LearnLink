9641653f4b222488ee7c0d6c43bcd2f292c2cc1e
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2024-07-14 10:36:08 +0600

    created quiz ui page

---
+++ LearnLink/Controllers/CreateQuizController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnLink.Controllers
+{
+    public class CreateQuizController : Controller
+    {
+        // GET: CreateQuiz
+        public ActionResult CreateQuiz()
+        {
+            return View();
+        }
+    }
+}
