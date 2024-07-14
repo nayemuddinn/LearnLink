687edb261c482aff264a84cd8dc82912b96f2e6a
Nayrit <nayritarn@gmail.com>
2024-07-15 03:37:38 +0600

    changeCredentials updated

---
+++ LearnLink/Controllers/changeCredentialsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnLink.Controllers
+{
+    public class changeCredentialsController : Controller
+    {
+        // GET: changeCredentials
+        public ActionResult changeCredentials()
+        {
+            return View();
+        }
+    }
+}
