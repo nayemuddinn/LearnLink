dd8d471b1e5ed10908f7684fea92ad4d3c2a1d1d
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2025-09-28 19:43:59 +0600

    Added Connection String for database Connection, Fixed  the DBConnection class

--- LearnLink/Content/DBconnection.cs
+++ LearnLink/Content/DBconnection.cs
@@ -1,11 +1,20 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Configuration;
 
 namespace LearnLink.Content
 {
-    public class DBconnection
+    public static class DBconnection
     {
+        public static string connStr = Init();
 
-       public static string connStr = "Data Source = DAREDEVIL\\SQLEXPRESS; Initial Catalog = learnlink; Integrated Security = True; TrustServerCertificate = True";
-
+        private static string Init()
+        {
+            var fromConfig = ConfigurationManager.ConnectionStrings["LearnLinkDb"]?.ConnectionString;
+            var fromEnv = Environment.GetEnvironmentVariable("LEARNLINK_CONNSTR");
+            var fallback = "Data Source=DAREDEVIL\\SQLEXPRESS; Initial Catalog=learnlink; Integrated Security=True; TrustServerCertificate=True";
+            if (!string.IsNullOrWhiteSpace(fromConfig)) return fromConfig;
+            if (!string.IsNullOrWhiteSpace(fromEnv)) return fromEnv;
+            return fallback;
+        }
     }
 }
